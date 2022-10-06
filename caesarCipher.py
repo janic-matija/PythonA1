3bd2ee5e1cd52a7096fa903320ad1d47226550bb
janic-matija <janic.j.matija@gmail.com>
2022-10-06 16:46:58 +0200

    TXT folder

--- caesarCipher.py
+++ caesarCipher.py
@@ -1,19 +1,15 @@
 import sys
 import os
 n=len(sys.argv)
-niz=['caesarCipher.py','e',1,'encodeme.txt','python']
-sys.argv=niz
-
-# for index in range (0,n):
-    # print(sys.argv[index])
-
+# niz=['caesarCipher.py','e',1,'encodeme.txt','python']
+# sys.argv=niz
 if sys.argv[1]!='e' and sys.argv[1]!='d':
     print("pogresan prvi parametar ('e' ili 'd')")
     exit()
 if int(sys.argv[2])<0 or int(sys.argv[2])>25:
     print("pogresan drugi parametar (izmedju 0 i 25)")
     exit()
-if  not os.path.isfile(sys.argv[3]):
+if  not os.path.isfile("TXT/"+sys.argv[3]):
     print("pogresan treci parametar (nije fajl)")
     exit()
 
@@ -22,13 +18,16 @@ mod=ord('z') - ord('a')  + 1 # mod = 26
 a=ord('a')
 
 if sys.argv[1]=='e' or sys.argv[1]=='d':
+    filePath="TXT/"
     shift = int(sys.argv[2])
     letterIndex=-1
-    readingFile=str(sys.argv[3])
+    readingFile=filePath+str(sys.argv[3])
     read = open(readingFile, 'r')
-    writingFile = "encoded.txt"
+    writingFile=filePath
     if sys.argv[1] == 'd':
-        writingFile = "decoded.txt"
+        writingFile += "decoded.txt"
+    else:
+        writingFile += "encoded.txt"
     write = open(writingFile, 'w')
     write = open(writingFile, 'a')
     for linija in read:
